11d79a7c0f4ec64b64a608d832093c74828183e3
stanislaw8 <pavlovinthesky@gmail.com>
2021-07-08 12:13:15 +0300

    game of life

---
+++ www/index.js
@@ -0,0 +1,11 @@
+import { Universe } from "life";
+
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+const renderLoop = () => {
+    pre.textContent = universe.render();
+    universe.tick();
+  
+    requestAnimationFrame(renderLoop);
+  };
+  requestAnimationFrame(renderLoop);
